0c3157279fca5b0a08d389fa6d348687efe190fa
Dominick Miceli <dcmiceli@syr.edu>
2026-09-03 14:38:02 -0400

    final

--- Lab2.py
+++ Lab2.py
@@ -1,14 +1,20 @@
 import streamlit as st
 from openai import OpenAI
+from pypdf import PdfReader
+
+def read_pdf(file):
+    reader = PdfReader(file)
+    string = ""
+    for page in reader.pages:
+        string += page.extract_text()
+    return string
 
 # Show title and description.
 st.title("MY Document question answering")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "Upload a pdf below and determine how you want it summarized – GPT will answer! "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.secrets.OPENAI_API_KEY
@@ -16,32 +22,48 @@ openai_api_key = st.secrets.OPENAI_API_KEY
 # Create an OpenAI client.
 client = OpenAI(api_key=openai_api_key)
 
+language = st.sidebar.selectbox('Select language:', [
+    'English',
+    'Spanish',
+    'French'
+])
+
+choice = st.sidebar.selectbox('Select one:', [
+    'Summarize the document in 100 words',
+    'Summarize the document in 2 paragraphs',
+    'Summarize the document in 5 bullet points'
+])
+
+check = st.sidebar.checkbox('Use advanced model')
+
+if check:
+    model='gpt-5-mini'
+else:
+    model='gpt-5-nano'
+
 # Let the user upload a file via `st.file_uploader`.
 uploaded_file = st.file_uploader(
-    "Upload a document (.txt or .md)", type=("txt", "md")
+    "Upload a document (.pdf)", type=("pdf")
 )
 
-# Ask the user for a question via `st.text_area`.
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
-
-if uploaded_file and question:
+if uploaded_file:
 
     # Process the uploaded file and question.
-    document = uploaded_file.read().decode()
+    document = read_pdf(uploaded_file)
     messages = [
         {
+            "role": "system",
+            "content": f"{choice}, in {language}"
+        },
+        {
             "role": "user",
-            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            "content": f"Summarize this document: {document}"
         }
     ]
 
     # Generate an answer using the OpenAI API.
     stream = client.chat.completions.create(
-        model="gpt-5-nano",
+        model=model,
         messages=messages,
         stream=True,
     )
